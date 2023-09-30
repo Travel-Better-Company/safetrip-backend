4273677bab5d73ffe275f0de078bfe88e9e3fcd9
NikoleGarc <nikoleupc@gmail.com>
2023-09-29 22:17:48 -0500

    Un comentario de lo que se hizo

--- safetrip-backend/src/main/java/com/safetripbackend/planner/domain/entity/itineraries.java
+++ safetrip-backend/src/main/java/com/safetripbackend/planner/domain/entity/itineraries.java
@@ -1,4 +1,11 @@
 package com.safetripbackend.planner.domain.entity;
 
 public class itineraries {
+    // Atributos
+    private int id;
+    private String nombre; // Ejemplo de atributo de tipo cadena (nombre del itinerario)
+    private Date fechaInicio; // Ejemplo de atributo de tipo Date (fecha de inicio)
+    private Date fechaFin; // Ejemplo de atributo de tipo Date (fecha de fin)
+    private int idUsuario; // Ejemplo de atributo de tipo entero (ID de usuario)
+    private int idCiudad;
 }
