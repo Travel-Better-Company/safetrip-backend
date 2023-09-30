c19df892a77bdfd845819567413185a57f109266
Felix Silva <82192868+Hostlop@users.noreply.github.com>
2023-09-29 22:24:00 -0500

    Update Users.java

--- safetrip-backend/src/main/java/com/safetripbackend/planner/domain/entity/Users.java
+++ safetrip-backend/src/main/java/com/safetripbackend/planner/domain/entity/Users.java
@@ -1,4 +1,8 @@
 package com.safetripbackend.planner.domain.entity;
 
 public class Users {
+    private int id;
+    private String name; // Ejemplo de atributo de tipo cadena
+    private String email; // Ejemplo de atributo de tipo cadena
+    private String password; // Ejemplo de atributo de tipo cadena
 }
