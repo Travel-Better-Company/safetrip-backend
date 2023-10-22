431729fb2d6b71cd3a9874170d9c0eff156df5fb
EstebanCabreraArbizu <u202014600@upc.edu.pe>
2023-10-21 20:51:08 -0500

    implementación de nikole

--- safetrip-backend/src/main/java/com/safetripbackend/PlannerItinerarie/service/ItineraryService.java
+++ safetrip-backend/src/main/java/com/safetripbackend/PlannerItinerarie/service/ItineraryService.java
@@ -1,17 +1,10 @@
 package com.safetripbackend.PlannerItinerarie.service;
 
 import com.safetripbackend.PlannerItinerarie.dto.*;
-<<<<<<< HEAD
-import com.safetripbackend.PlannerItinerarie.entity.Activities;
-import com.safetripbackend.PlannerItinerarie.entity.Itineraries;
-import com.safetripbackend.PlannerItinerarie.mappers.ActivityMapper;
-import com.safetripbackend.PlannerItinerarie.repository.ActivityRepository;
-=======
 import com.safetripbackend.PlannerItinerarie.entity.Itineraries;
 import com.safetripbackend.PlannerItinerarie.exception.ResourceAlreadyExistsException;
 import com.safetripbackend.PlannerItinerarie.exception.ResourceNotFoundException;
 import com.safetripbackend.PlannerItinerarie.mappers.ItineraryMapper;
->>>>>>> 7f298034ff1720814bfbe30697ac56ab22ff26bf
 import com.safetripbackend.PlannerItinerarie.repository.ItineraryRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.ConstraintViolation;
@@ -30,27 +23,6 @@ public class ItineraryService {
     private final ItineraryRepository itineraryRepository;
     private final ItineraryMapper itineraryMapper;
     private final Validator validator;
-<<<<<<< HEAD
-    /*
-    public ItineraryService(Validator validator){
-        this.validator = validator;
-        this.loadElements();
-    }*/
-    private void loadElements(){
-        /*Falta cargar todos los elementos*/
-        UserResponseDto user1 = new UserResponseDto();
-        user1.setId(1);
-        user1.setName("Alfredo");
-        user1.setEmail("123@gmial.com");
-        user1.setPassword("13245");
-        ItineraryResponseDto itinerarie = new ItineraryResponseDto();
-        itinerarie.setId(2);
-        itinerarie.setName("Viaje de playas");
-
-        ActivityResponseDto activitie1 = new ActivityResponseDto();
-        activitie1.setId(3);
-        activitie1.setName("Juegos de playa en cartagena");
-=======
     @Transactional
     public ItineraryResponseDto createItinerary(ItineraryRequestDto itineraryResource) {
         if(itineraryRepository.existsByName(itineraryResource.getName())){
@@ -78,7 +50,6 @@ public class ItineraryService {
         } else {
             throw new ResourceNotFoundException("Itinerario con ese id no existe"+itineraryId);
         }
->>>>>>> 7f298034ff1720814bfbe30697ac56ab22ff26bf
     }
     @Transactional
     public  void deleteItinerary(Long itineraryId){
@@ -88,22 +59,6 @@ public class ItineraryService {
 
         itineraryRepository.deleteById(itineraryId);
     }
-<<<<<<< HEAD
-    /*
-    public List<Itineraries> findByDestination(String destination) {
-        List<Itineraries> itinerariesByDestination = new ArrayList<>();
-        for (Itineraries itinerary : itineraries) {
-            if (itinerary.getDestination().equalsIgnoreCase(destination)) {
-                itinerariesByDestination.add(itinerary);
-            }
-        }
-        return itinerariesByDestination;
-
-    }*/
-
-
-
-=======
     public List<ItineraryResponseDto> getAllItinerary() {
         List<Itineraries> itinerary = itineraryRepository.findAll();
         return itineraryMapper.entityListToResponseResourceList(itinerary);
@@ -118,5 +73,4 @@ public class ItineraryService {
 
         return itineraryMapper.entityListToResponseResourceList(itinerariesByDestination);
     }
->>>>>>> 7f298034ff1720814bfbe30697ac56ab22ff26bf
 }
