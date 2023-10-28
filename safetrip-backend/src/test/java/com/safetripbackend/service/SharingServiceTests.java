60f778c88a7db002c5d25c649425d73389d83912
FrancoSimonini <81375893+FrancoSimonini@users.noreply.github.com>
2023-10-27 22:41:54 -0500

    Test de la funcion de Sharing
    Funcion testeada con exito.
    
    Signed-off-by: FrancoSimonini <81375893+FrancoSimonini@users.noreply.github.com>

---
+++ safetrip-backend/src/test/java/com/safetripbackend/service/SharingServiceTests.java
@@ -0,0 +1,45 @@
+package com.safetripbackend;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+
+import com.safetripbackend.service.ItineraryService;
+import com.safetripbackend.controller.ItineraryController;
+import com.safetripbackend.dto.SharingResponseDto;
+
+import static org.mockito.Mockito.doNothing;
+
+@ExtendWith(MockitoExtension.class)
+public class SharingServiceTests {
+
+    @InjectMocks
+    private ItineraryController mockController;
+
+    @Mock
+    private ItineraryService itineraryService;
+
+    @Test
+    public void testShareItinerary() {
+        // Parametros input
+        long id_itinerary = 1;
+        long id_user = 2;
+        long id_target = 3;
+
+        // Creando Mock
+        SharingResponseDto mockResponse = new SharingResponseDto();
+        mockResponse.setText("Itinerary shared successfully");
+
+        // mock de servicio
+        ResponseEntity<SharingResponseDto> response = mockController.share_itinerary(id_itinerary, id_user, id_target);
+
+        // assert response
+        assert response.getStatusCode().equals(HttpStatus.OK);
+    }
+}
