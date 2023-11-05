8f821f15403f3fd61b1f5ee0e936591c76a32c6e
FrancoSimonini <81375893+FrancoSimonini@users.noreply.github.com>
2023-11-05 00:07:50 -0500

    Actualizando Service de Subscripcion
    Resolviendo el error de registro de user en createSusbscripcion
    Modificando mapToResponseDTO para que en ResponseDTO devuelva todos los datos del usuario
    Añadiendo deleteSubscription para eliminar subscripciones.
    
    Signed-off-by: FrancoSimonini <81375893+FrancoSimonini@users.noreply.github.com>

--- safetrip-backend/src/main/java/com/safetripbackend/service/SubscriptionService.java
+++ safetrip-backend/src/main/java/com/safetripbackend/service/SubscriptionService.java
@@ -2,12 +2,17 @@ package com.safetripbackend.service;
 
 import com.safetripbackend.dto.SubscriptionRequestDTO;
 import com.safetripbackend.dto.SubscriptionResponseDTO;
+import com.safetripbackend.exception.ResourceNotFoundException;
 import com.safetripbackend.repository.UserRepository;
+import com.safetripbackend.entity.Users;
 import com.safetripbackend.entity.Subscription;
 import com.safetripbackend.repository.SubscriptionRepository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 
@@ -25,20 +30,26 @@ public class SubscriptionService {
         return subscriptionRepository.existsById(subscriptionId);
     }
     public SubscriptionResponseDTO createSubscription(SubscriptionRequestDTO requestDTO) {
+        if (!userRepository.existsById(requestDTO.getUserId())) {
+            throw new IllegalArgumentException("User does not exist for ID: " + requestDTO.getUserId());
+        }
+
+        // Proceed with creating a new subscription
         Subscription subscription = new Subscription();
-        subscription.setUser(userRepository.findById(requestDTO.getUserId()).orElse(null));
+        Users user = userRepository.findById(requestDTO.getUserId())
+                .orElseThrow(() -> new NoSuchElementException("User not found for ID: " + requestDTO.getUserId()));
+        subscription.setUser(user);
         subscription.setStartDate(requestDTO.getStartDate());
         subscription.setEndDate(requestDTO.getEndDate());
-        Subscription savedSubscription = subscriptionRepository.save(subscription);
-        return mapToResponseDTO(savedSubscription);
+        subscriptionRepository.save(subscription);
+        //Subscription savedSubscription = subscriptionRepository.save(subscription);
+        return mapToResponseDTO(subscription);
     }
 
     public SubscriptionResponseDTO getSubscriptionById(Long subscriptionId) {
-        Subscription subscription = subscriptionRepository.findById(subscriptionId).orElse(null);
-        if (subscription != null) {
-            return mapToResponseDTO(subscription);
-        }
-        return null;
+        Subscription subscription = subscriptionRepository.findById(subscriptionId)
+                .orElseThrow(() -> new NoSuchElementException("Subscription not found for ID: " + subscriptionId));
+        return mapToResponseDTO(subscription);
     }
 
     public List<SubscriptionResponseDTO> getAllSubscriptions() {
@@ -48,10 +59,19 @@ public class SubscriptionService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public  void deleteSubscription(long subs_id){
+        if (!subscriptionRepository.existsById(subs_id)) {
+            throw new ResourceNotFoundException("Itinerario con ese id no existe"+subs_id);
+        }
+        subscriptionRepository.deleteById(subs_id);
+    }
+
     private SubscriptionResponseDTO mapToResponseDTO(Subscription subscription) {
         SubscriptionResponseDTO responseDTO = new SubscriptionResponseDTO();
         responseDTO.setId(subscription.getId());
-        responseDTO.setUserId(subscription.getUser().getId());
+        Users user_a = subscription.getUser();
+        responseDTO.setUser_register(user_a);
         responseDTO.setStartDate(subscription.getStartDate());
         responseDTO.setEndDate(subscription.getEndDate());
         return responseDTO;
