3b70339d099a42dbbb0b3f831680eec6e2359481
NikoleGarc <nikoleupc@gmail.com>
2023-11-09 11:35:52 -0500

    Arreglé el SuscriptionServiceTest Y modifiqué el service cambiando un operador logico

--- safetrip-backend/src/test/java/com/safetripbackend/service/SubscriptionServiceTest.java
+++ safetrip-backend/src/test/java/com/safetripbackend/service/SubscriptionServiceTest.java
@@ -1,20 +1,16 @@
 package com.safetripbackend.service;
-
 import com.safetripbackend.dto.SubscriptionRequestDTO;
 import com.safetripbackend.dto.SubscriptionResponseDTO;
 import com.safetripbackend.repository.UserRepository;
 import com.safetripbackend.entity.Subscription;
 import com.safetripbackend.entity.Users;
-import com.safetripbackend.service.SubscriptionService;
 import com.safetripbackend.repository.SubscriptionRepository;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import java.time.LocalDateTime;
-
+import java.time.LocalDate;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,13 +18,10 @@ public class SubscriptionServiceTest {
 
     @InjectMocks
     private SubscriptionService subscriptionService;
-
     @Mock
     private SubscriptionRepository subscriptionRepository;
-
     @Mock
     private UserRepository userRepository;
-
     @BeforeEach
     public void init() {
         MockitoAnnotations.initMocks(this);
@@ -37,9 +30,9 @@ public class SubscriptionServiceTest {
     @Test
     public void testCreateSubscription() {
         SubscriptionRequestDTO requestDTO = new SubscriptionRequestDTO();
-        requestDTO.setUserId(1L);
-        requestDTO.setStartDate(LocalDateTime.now());
-        requestDTO.setEndDate(LocalDateTime.now().plusDays(30));
+        requestDTO.setUserId(3L);
+        requestDTO.setStartDate(LocalDate.now());
+        requestDTO.setEndDate(LocalDate.now().plusDays(30));
 
         Subscription subscription = new Subscription();
         subscription.setUser(new Users());
@@ -47,6 +40,7 @@ public class SubscriptionServiceTest {
         subscription.setEndDate(requestDTO.getEndDate());
 
         when(userRepository.findById(requestDTO.getUserId())).thenReturn(java.util.Optional.of(new Users()));
+
         when(subscriptionRepository.save(any(Subscription.class))).thenReturn(subscription);
 
         SubscriptionResponseDTO responseDTO = subscriptionService.createSubscription(requestDTO);
